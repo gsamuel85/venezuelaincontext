6a9823dd59335c8ec0b1e452aa2ee907f5c8bc5b
Guy Samuel <gsamuel85@gmail.com>
2016-02-09 17:23:14 +0000

    add realtime replies to comments

--- client/js/commentCtrl.js
+++ client/js/commentCtrl.js
@@ -24,18 +24,49 @@ app.controller('CommentCtrl', ['$scope', function($scope) {
         xhr.send();
     };
     
+    var addCommentToTree = function(comment) {
+        var path = comment.path.split(",");
+        
+        console.log(path);
+        
+        if (path[1] === "") {
+            $scope.$apply(function() {
+                $scope.comments.push(comment);
+            });
+        } else {
+            // Find parent comment
+            $scope.comments.forEach(function(item) {
+                if (item._id === path[1]) {
+                    $scope.$apply(function() {
+                        if (!item.replies) { item.replies = []; }
+                        item.replies.push(comment);
+                    });
+                }
+            });
+        }
+    };
+    
     var socket = io();
     
     socket.on('add comment', function(comment) {
-        $scope.$apply(function() {
-            $scope.comments.push(comment);
-        });
+        addCommentToTree(comment);
     });
     
     $scope.sendComment = function() {
         socket.emit('add comment', $scope.newComment);
         
         $scope.newComment.text = '';        // Reset field
+    };
+    
+    $scope.sendReply = function(comment) {
+        var reply = {
+            video_id: $scope.video._id,
+            text: comment.replyText,
+            parent_id: comment._id
+        };
+        
+        socket.emit('add comment', reply);
+        comment.replyText = '';
     };
     
     
