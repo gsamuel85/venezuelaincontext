7393a3653a367c0e5e0fb87b22a7df4da79c6ad8
Guy Samuel <guy@capitmobile.com>
2016-07-12 15:27:27 +0200

    update db config mLab - test authDB

--- config/db.js
+++ config/db.js
@@ -7,7 +7,7 @@ module.exports = function(user, password) {
         },
         prod: {
             url: "mongodb://" + user + 
-            ":" + password + "@ds013898.mongolab.com:13898/venezuelaincontext"
+            ":" + password + "@ds013898.mongolab.com:13898/venezuelaincontext?authSource=dbWithUserCredentials"
         }
     };
 };
